ec6da0a611994cdbee404f0c33794e9736b7df71
Tonashiro <vinit.tonashiro@hotmail.com>
2025-05-19 20:00:38 -0300

    fix: update hero content

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 import { AddProjectCard } from "@/components/AddProjectCard";
 import { useUserContext } from "@/contexts/userContext";
+import Link from "next/link";
 
 /**
  * The `Hero` component is a React functional component that renders a hero section
@@ -31,19 +31,21 @@ export const Hero: React.FC = () => {
     <section className="text-center max-w-3xl mx-auto relative mt-[64px] mb-[5%] pt-[5%]">
       <div className="relative">
         <h2 className="text-3xl sm:text-5xl line-height-normal font-bold bg-gradient-to-r from-purple-300 via-purple-500 to-indigo-400 bg-clip-text text-transparent mb-5 leading-[1.15]">
-          Naddify
+          Vote. Verify. Discover.
         </h2>
         <p className="text-lg sm:text-xl text-gray-300 leading-relaxed max-w-2xl mx-auto font-medium">
-          A platform for community-led project discovery on
+          Naddify is a platform for community-led project discovery on{" "}
+          <Link
+            href="https://testnet.monad.xyz"
+            target="_blank"
+            className="text-[#836EF9]"
+          >
+            {" "}
+            Monad.
+          </Link>{" "}
+          Trusted community members vote and verify, helping you discover what
+          truly deserves attention.
         </p>
-        <Image
-          alt="Monad Logo"
-          src="/images/monad-logo.svg"
-          width={140}
-          height={28}
-          className="h-6 sm:h-7 w-auto flex mx-auto mt-6"
-          priority
-        />
       </div>
 
       {user?.is_admin && <AddProjectCard />}
